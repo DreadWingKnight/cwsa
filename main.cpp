4250dd961921569dc81eb77003e50a0972de2c60
Harold Feit <DreadWingKnight@users.noreply.github.com>
2006-02-07 18:06:54 +0000

    Initial Commit of WebSeed Adder

---
+++ main.cpp
@@ -0,0 +1,65 @@
+#include "atom.h"
+#include "bencode.h"
+#include "util.h"
+#include "stringsplit.h"
+#include "WebSeedAdder_CPP_private.h"
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    if( !argv[1] || !argv[2] )
+    {
+	 	printf( "%s %s\n", FILE_DESCRIPTION, VER_STRING);
+	 	printf("You failed to include one or more required command line arguments.\n\n");
+	 	printf("WebSeedAdder-Console <Torrent File> [--include_existing] <webseeds>\n");
+    }
+    else
+    {
+        for( int t=2; t<argc; t++ )
+        {
+	 	CAtom *pTorrent;
+	 	pTorrent = DecodeFile( argv[t] );
+	 	if( pTorrent->isDicti() )
+	 	{
+		 	if( string( argv[1] ).find( "http://" ) == string ::npos )
+		 	{
+			 	printf( "Removing Seed\n" );
+			 	((CAtomDicti *)pTorrent)->delItem("httpseeds" );
+			}
+			else
+			{
+			 	printf( "Adding Seed" );
+ 	 		 	CAtomList *pWebSeedList;
+		 	 	pWebSeedList = new CAtomList();
+  		     	string sWebSeedParamater;
+		 	 	sWebSeedParamater = string( argv[1] );
+			 	if( sWebSeedParamater.find_first_of( ";") == string :: npos )
+			 	{
+                    printf( "\n" );
+			 		pWebSeedList->addItem( new CAtomString( sWebSeedParamater ) );
+			 	}
+			 	else
+			 	{
+                    printf( "s\n");
+			 		vector<string> v_sWebSeeds;
+			 		StringUtils::SplitString( sWebSeedParamater, ";", v_sWebSeeds, false);
+  			    	for( vector<string> :: iterator i = v_sWebSeeds.begin(); i != v_sWebSeeds.end( ); i++ )
+  			    	{
+					 	 if( (*i).find("http://") != string::npos )
+				 		 pWebSeedList->addItem( new CAtomString( (*i) ) );
+					}
+ 			 	}
+			 	((CAtomDicti *)pTorrent)->setItem( "httpseeds", pWebSeedList );
+			}
+			UTIL_MakeFile( argv[t] , Encode( pTorrent ) );
+		 }
+		 delete pTorrent;
+       }
+	}
+	system("PAUSE");
+    return EXIT_SUCCESS;
+}
