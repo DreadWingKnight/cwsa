3483859d6fdf52fa11b9012323b8a3329f56e96a
DWKnight <DWKnight@134542f7-61da-c646-bebc-32e8e8abdeed>
2006-02-07 18:06:54 +0000

    Initial Commit of WebSeed Adder

---
+++ WebSeedAdder_CPP_private.h
@@ -0,0 +1,23 @@
+/* THIS FILE WILL BE OVERWRITTEN BY DEV-C++ */
+/* DO NOT EDIT ! */
+
+#ifndef WEBSEEDADDER_CPP_PRIVATE_H
+#define WEBSEEDADDER_CPP_PRIVATE_H
+
+/* VERSION DEFINITIONS */
+#define VER_STRING	"0.1.0.1"
+#define VER_MAJOR	0
+#define VER_MINOR	1
+#define VER_RELEASE	0
+#define VER_BUILD	1
+#define COMPANY_NAME	""
+#define FILE_VERSION	""
+#define FILE_DESCRIPTION	"C++ WebSeed Adder"
+#define INTERNAL_NAME	""
+#define LEGAL_COPYRIGHT	"2006 Harold Feit. Portions Copyright 2003-2006 Trevor Hogan"
+#define LEGAL_TRADEMARKS	""
+#define ORIGINAL_FILENAME	"WebSeedAdder-Console.exe"
+#define PRODUCT_NAME	"C++ WebSeed Adder"
+#define PRODUCT_VERSION	""
+
+#endif /*WEBSEEDADDER_CPP_PRIVATE_H*/
