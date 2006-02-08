31e77a40d0378c38edc656810ceedea20b7bc30f
DWKnight <DWKnight@134542f7-61da-c646-bebc-32e8e8abdeed>
2006-02-08 14:57:14 +0000

    Include support to add new webseeds without removing existing ones.

--- main.cpp
+++ main.cpp
@@ -15,51 +15,82 @@ int main(int argc, char *argv[])
     {
 	 	printf( "%s %s\n", FILE_DESCRIPTION, VER_STRING);
 	 	printf("You failed to include one or more required command line arguments.\n\n");
-	 	printf("WebSeedAdder-Console <webseeds> <Torrent File> \n");
-	 	printf("<webseeds> must contain at least one http:// address\nor the webseeds will be removed from the file.\n");
-	 	printf("If you do not include the <webseeds> paramater,\nthe file will most likely NOT be edited.\n");
+	 	printf("WebSeedAdder-Console <webseeds> [--include_existing] <Torrent File> [Torrent File [...]] \n\n");
+	 	printf("<webseeds> must contain at least one http:// address\nor the webseeds will be removed from the file.\n\n");
+	 	printf("If you do not include the <webseeds> paramater,\nthe file will most likely NOT be edited.\n\n");
+	 	printf("The <webseeds> paramater is a list of http:// addresses separated by \";\"\n");
+	 	printf("Any number of webseeds may be added. Existing webseeds in the torrent will be removed.\n");
     }
     else
     {
+        bool include_existing;
         for( int t=2; t<argc; t++ )
         {
-	 	CAtom *pTorrent;
-	 	pTorrent = DecodeFile( argv[t] );
-	 	if( pTorrent->isDicti() )
-	 	{
-		 	if( string( argv[1] ).find( "http://" ) == string ::npos )
-		 	{
-			 	printf( "Removing Seed\n" );
-			 	((CAtomDicti *)pTorrent)->delItem("httpseeds" );
-			}
-			else
-			{
-			 	printf( "Adding Seed" );
- 	 		 	CAtomList *pWebSeedList;
-		 	 	pWebSeedList = new CAtomList();
-  		     	string sWebSeedParamater;
-		 	 	sWebSeedParamater = string( argv[1] );
-			 	if( sWebSeedParamater.find_first_of( ";") == string :: npos )
-			 	{
-                    printf( "\n" );
-			 		pWebSeedList->addItem( new CAtomString( sWebSeedParamater ) );
-			 	}
-			 	else
-			 	{
-                    printf( "s\n");
-			 		vector<string> v_sWebSeeds;
-			 		StringUtils::SplitString( sWebSeedParamater, ";", v_sWebSeeds, false);
-  			    	for( vector<string> :: iterator i = v_sWebSeeds.begin(); i != v_sWebSeeds.end( ); i++ )
-  			    	{
-					 	 if( (*i).find("http://") != string::npos )
-				 		 pWebSeedList->addItem( new CAtomString( (*i) ) );
-					}
- 			 	}
-			 	((CAtomDicti *)pTorrent)->setItem( "httpseeds", pWebSeedList );
-			}
-			UTIL_MakeFile( argv[t] , Encode( pTorrent ) );
-		 }
-		 delete pTorrent;
+             if( t == 2 && string( argv[t] ) == string( "--include_existing" ) )
+             {
+                 include_existing = true;
+             }
+             else
+             {
+                 CAtom *pTorrent;
+	 	         pTorrent = DecodeFile( argv[t] );
+	 	         if( pTorrent->isDicti() )
+	 	         {
+		 	         if( string( argv[1] ).find( "http://" ) == string ::npos )
+		 	         {
+			 	         printf( "Removing Seed\n" );
+			 	         ((CAtomDicti *)pTorrent)->delItem("httpseeds" );
+		             }
+                 else
+			     {
+			 	     printf( "Adding Seed" );
+                     CAtomList *pWebSeedList = new CAtomList();
+			 	     if( include_existing == true )
+			 	     {
+                         printf("\nChecking for existing seeds");
+                         CAtom *pExisting;
+                         pExisting = ((CAtomDicti *)pTorrent)->getItem( "httpseeds" , NULL );
+                         if ( pExisting && pExisting->isList( ) )
+                         {
+                              printf(", Existing list present");
+                              pWebSeedList = (CAtomList *)pExisting;
+                         }
+                         else
+                         {
+                             printf(", Existing list not present");
+                             pWebSeedList = new CAtomList();
+                         }
+                     }
+                     else
+                     {
+                         pWebSeedList = new CAtomList();
+                     }
+                     printf("\nAdding new seed");
+                     string sWebSeedParamater;
+		 	 	     sWebSeedParamater = string( argv[1] );
+			 	     if( sWebSeedParamater.find_first_of( ";") == string :: npos )
+			 	     {
+                         printf( " to torrent\n" );
+			 		     pWebSeedList->addItem( new CAtomString( sWebSeedParamater ) );
+			 	     }
+			 	     else
+			 	     {
+                          printf( "s to torrent\n");
+                          vector<string> v_sWebSeeds;
+			 		      StringUtils::SplitString( sWebSeedParamater, ";", v_sWebSeeds, false);
+  			    	      for( vector<string> :: iterator i = v_sWebSeeds.begin(); i != v_sWebSeeds.end( ); i++ )
+  			    	      {
+					 	       if( (*i).find("http://") != string::npos )
+				 		       pWebSeedList->addItem( new CAtomString( (*i) ) );
+					      }
+ 			 	     }
+ 			 	     if( include_existing == true && ((CAtomDicti *)pTorrent)->getItem( "httpseeds" , NULL ) == NULL )
+			 	     ((CAtomDicti *)pTorrent)->setItem( "httpseeds", pWebSeedList );
+		         }
+			     UTIL_MakeFile( argv[t] , Encode( pTorrent ) );
+	         }
+		     delete pTorrent;
+           }
        }
 	}
 #ifdef WIN32
