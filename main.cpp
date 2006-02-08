9cffc3a9090374e0132cc064926126df00212111
Harold Feit <DreadWingKnight@users.noreply.github.com>
2006-02-08 20:10:37 +0000

    Update to support wsa.configure file for passing webseed information.

--- main.cpp
+++ main.cpp
@@ -15,20 +15,35 @@ int main(int argc, char *argv[])
     {
 	 	printf( "%s %s\n", FILE_DESCRIPTION, VER_STRING);
 	 	printf("You failed to include one or more required command line arguments.\n\n");
-	 	printf("WebSeedAdder-Console <webseeds> [--include_existing] <Torrent File> [Torrent File [...]] \n\n");
-	 	printf("<webseeds> must contain at least one http:// address\nor the webseeds will be removed from the file.\n\n");
-	 	printf("If you do not include the <webseeds> paramater,\nthe file will most likely NOT be edited.\n\n");
+	 	printf("WebSeedAdder-Console <webseeds/--use_wsa_config> [--include_existing] <Torrent\nFile> [Torrent File [...]] \n\n");
+	 	printf("<webseeds> must contain at least one http:// address or the webseeds will be\nremoved from the file unless the --use_wsa_config paramater is passed.\n\n");
+	 	printf("If you do not include the <webseeds> paramater, the file will most likely NOT\nbe edited.\n\n");
 	 	printf("The <webseeds> paramater is a list of http:// addresses separated by \";\"\n");
-	 	printf("Any number of webseeds may be added. Existing webseeds in the torrent will be removed.\n");
+	 	printf("Any number of webseeds may be added. Existing webseeds in the torrent will be\nremoved unless the --include_existing paramater is passed.\n");
+	 	printf("The --use_wsa_config paramater uses the wsa.configure file from EAD's\nVB.Net Webseed adder application.\n" );
     }
     else
     {
+        CAtom *pWSAConfig;
+#ifdef WIN32
+        pWSAConfig = DecodeFile( ".\\wsa.configure" );
+#else
+        pWSAConfig = DecodeFile( "./wsa.configure" );
+#endif
+        CAtomList *pWSAConfigList = new CAtomList();
+        if( pWSAConfig->isDicti() )
+        {
+            CAtom *pCheck;
+            pCheck = ((CAtomDicti *)pWSAConfig)->getItem( "seeds" , NULL);
+            if( pCheck && pCheck->isList() )
+                pWSAConfigList = (CAtomList *)pCheck;
+        }
         bool include_existing;
         for( int t=2; t<argc; t++ )
         {
              if( t == 2 && string( argv[t] ) == string( "--include_existing" ) )
              {
-                 include_existing = true;
+                 continue;
              }
              else
              {
@@ -36,7 +51,7 @@ int main(int argc, char *argv[])
 	 	         pTorrent = DecodeFile( argv[t] );
 	 	         if( pTorrent->isDicti() )
 	 	         {
-		 	         if( string( argv[1] ).find( "http://" ) == string ::npos )
+		 	         if( string( argv[1] ).find( "http://" ) == string ::npos && string(argv[1]) != string("--use_wsa_config") )
 		 	         {
 			 	         printf( "Removing Seed\n" );
 			 	         ((CAtomDicti *)pTorrent)->delItem("httpseeds" );
@@ -45,7 +60,7 @@ int main(int argc, char *argv[])
 			     {
 			 	     printf( "Adding Seed" );
                      CAtomList *pWebSeedList = new CAtomList();
-			 	     if( include_existing == true )
+			 	     if( string( argv[2] ) == string( "--include_existing" ) )
 			 	     {
                          printf("\nChecking for existing seeds");
                          CAtom *pExisting;
@@ -59,21 +74,36 @@ int main(int argc, char *argv[])
                          {
                              printf(", Existing list not present");
                              pWebSeedList = new CAtomList();
+                             ((CAtomDicti *)pTorrent)->setItem( "httpseeds", pWebSeedList );
                          }
                      }
                      else
                      {
+                         printf( ", Removing existing seeds" );
+   			 	         ((CAtomDicti *)pTorrent)->delItem( "httpseeds" );
                          pWebSeedList = new CAtomList();
+                         ((CAtomDicti *)pTorrent)->setItem( "httpseeds", pWebSeedList );
                      }
                      printf("\nAdding new seed");
                      string sWebSeedParamater;
 		 	 	     sWebSeedParamater = string( argv[1] );
-			 	     if( sWebSeedParamater.find_first_of( ";") == string :: npos )
+			 	     if( sWebSeedParamater.find_first_of( ";") == string :: npos && string(argv[1]) != string("--use_wsa_config") )
 			 	     {
                          printf( " to torrent\n" );
 			 		     pWebSeedList->addItem( new CAtomString( sWebSeedParamater ) );
 			 	     }
-			 	     else
+			 	     else if (string(argv[1]) == string("--use_wsa_config") )
+			 	     {
+                          printf( "(s) from wsa.config to torrent\n");
+                          vector<CAtom *> v_aWebSeeds;
+                          v_aWebSeeds = pWSAConfigList->getValue();
+                          for( vector<CAtom *> :: iterator i = v_aWebSeeds.begin(); i != v_aWebSeeds.end(); i++ )
+                          {
+                               printf( ".");
+                               pWebSeedList->addItem( (*i) );
+                          }
+                     }
+                     else
 			 	     {
                           printf( "s to torrent\n");
                           vector<string> v_sWebSeeds;
@@ -84,9 +114,8 @@ int main(int argc, char *argv[])
 				 		       pWebSeedList->addItem( new CAtomString( (*i) ) );
 					      }
  			 	     }
- 			 	     if( include_existing == true && ((CAtomDicti *)pTorrent)->getItem( "httpseeds" , NULL ) == NULL )
-			 	     ((CAtomDicti *)pTorrent)->setItem( "httpseeds", pWebSeedList );
 		         }
+		         printf( "\n");
 			     UTIL_MakeFile( argv[t] , Encode( pTorrent ) );
 	         }
 		     delete pTorrent;
