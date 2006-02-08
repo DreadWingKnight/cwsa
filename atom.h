08645f99eef776f9170c618f5ab57de7a4dfb71e
Harold Feit <DreadWingKnight@users.noreply.github.com>
2006-02-08 14:05:50 +0000

    Update for Linux Compatibility

--- atom.h
+++ atom.h
@@ -13,8 +13,14 @@
 
 using namespace std;
 
+#ifdef WIN32
  typedef __int64 int64;
  typedef unsigned __int64 uint64;
+#else
+ typedef long long int64;
+ typedef unsigned long long uint64;
+#endif
+
 
 class CAtom
 {
